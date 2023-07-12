6825e3be9a1dce5a3c51d9ae5f6dadf8f38f3b80
Vinicius Guerra <vini.fms@hotmail.com>
2023-07-12 19:25:52 -0300

    CRUD paciente

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -18,16 +18,33 @@ public class Paciente {
     private Long id;
     private String nome;
     private String email;
+    private Boolean ativo;
     private String telefone;
     private String cpf;
     @Embedded
     private Endereco endereco;
 
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null)
+            this.nome = dados.nome();
+
+        if (dados.telefone() != null)
+            this.telefone = dados.telefone();
+
+        if (dados.endereco() != null)
+            endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void inativar() {
+        this.ativo = false;
     }
 }
